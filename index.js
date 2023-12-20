87f5a0921a3fba09f957146dc66e53ad8ed8280c
BrandBlood97 <brandon1bloodsworth@gmail.com>
2023-12-19 22:32:57 -0500

    Developed working code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 // TODO: Create an array of questions for user input
@@ -54,60 +55,19 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { 
-    const readmeContent = `
-# ${data.title}
-
-## Description
-${data.description}
-
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
-## Installation
-${data.installation}
-
-## Usage
-${data.usage}
-
-## License
-This application is covered under the ${data.license} license.
-
-## Contributing
-${data.contributing}
-
-## Tests
-${data.tests}
-
-## Questions
-For additional questions, you can reach me through:
-- GitHub: [${data.github}](https://github.com/${data.github})
-- Email: ${data.email}
-`;
-
-    // Write the README file
-    fs.writeFile(fileName, readmeContent, (err) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log('README file generated successfully!');
-        }
-    });
-}
-
-// TODO: Create a function to initialize app
-function init() {inquirer
-        .prompt(questions)
-        .then((answers) => {
-            // Call the writeToFile function with the provided answers
-            writeToFile('README.md', answers);
-        });
-}
-
-// Function call to initialize app
-init();
+ function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+}    
+    // TODO: Create a function to initialize app
+    function init() {inquirer
+            .prompt(questions)
+            .then((answers) => {
+                // Call the writeToFile function with the provided answers
+                writeToFile('README.md', generateMarkdown(answers));
+            });
+    }
+    
+    // Function call to initialize app
+    init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,74 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const link = renderLicenseLink(license);
+  if (link === '') {
+    return '';
+  }
+  return `## License
+This application is covered under the ${license} license. For more information, visit ${link}!`;
+}
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+For additional questions, you can reach me through:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
 `;
 }
 
